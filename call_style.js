26b07dc363913ad45ab99ebac40fc80e61224e9b
Muhammad-Subhan-Rauf <raufsubhan45@gmail.com>
2025-02-05 20:08:59 +0500

    Made the chatbot slightly more responsive

--- call_style.js
+++ call_style.js
@@ -12,22 +12,30 @@ document.addEventListener('DOMContentLoaded', () => {
 const messengerId = "BotPenguin-messenger";
 const launcherId = "botpenguin-launcher-12";
 
+
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         if (mutation.type === "attributes" && mutation.attributeName === "style") {
+            let temp_element = document.getElementById("botpenguin-launcher-text-12");
             const messenger = document.getElementById(messengerId);
             const launcher = document.getElementById(launcherId);
+            console.log(temp_element);
+            
+            // Check screen size and hide temp_element only if it's less than 768px
+            if (window.innerWidth < 768) {
+                temp_element.style.display = "none";
+            }
 
             if (messenger && launcher && messenger.style.display === "block") {
                 console.log("BotPenguin messenger detected. Hiding it...");
                 messenger.style.display = "none"; // Hide the messenger
                 launcher.style.display = "block"; // Show the launcher
                 observer.disconnect();
-
             }
         }
     });
 });
+
 
 // Function to start observing once the elements are available
 function observeElement() {
